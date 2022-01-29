a9984a19b4d65cbd0e9f93001d743217205fb832
bhq <bhq@op.pl>
2022-01-30 00:34:38 +0100

    Main character animations tweak.

--- Assets/Scripts/Animations/MainCharacterAnimationController.cs
+++ Assets/Scripts/Animations/MainCharacterAnimationController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class MainCharacterAnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private Vector3Event DuringShootInput;
+	[SerializeField]
+	private Vector3Event ShootInputFinished;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private GameObject IdleSprite;
+	[SerializeField]
+	private GameObject FocusSprite;
+	[SerializeField]
+	private GameObject ActionSprite;
+
+	[SerializeField]
+	private float ActionSpriteShowLength;
+
+	private void Awake()
+	{
+		DuringShootInput.Action += OnDuringShootInput;
+		ShootInputFinished.Action += OnShootInputFinished;
+	}
+
+	private void OnDestroy()
+	{
+		DuringShootInput.Action -= OnDuringShootInput;
+		ShootInputFinished.Action -= OnShootInputFinished;
+	}
+
+	private void OnDuringShootInput(Vector3 _)
+	{
+		IdleSprite.SetActive(false);
+		FocusSprite.SetActive(true);
+	}
+
+	private void OnShootInputFinished(Vector3 _)
+	{
+		FocusSprite.SetActive(false);
+		ActionSprite.SetActive(true);
+
+		StartCoroutine(RestoreIdleSpriteAfterDelay());
+	}
+
+	private IEnumerator RestoreIdleSpriteAfterDelay()
+	{
+		yield return new WaitForSeconds(ActionSpriteShowLength);
+
+		ActionSprite.SetActive(false);
+		IdleSprite.SetActive(true);
+	}
 }
