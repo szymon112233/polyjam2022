9e63a524f481a54e836730afbe8ff04ff51b349f
bhq <bhq@op.pl>
2022-01-30 00:19:08 +0100

    Added gameplay music. Working on final animations of main character.

---
+++ Assets/Scripts/Animations/AnimatorBetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorBetter : MonoBehaviour
+{
+	public List<Sprite> FrameSprites = new List<Sprite>();
+	public float FrameTime;
+
+	private SpriteRenderer SpriteRenderer;
+
+	private int frameIndex;
+	private float timer;
+
+	private void Awake()
+	{
+		frameIndex = 0;
+		timer = 0;
+		SpriteRenderer = GetComponent<SpriteRenderer>();
+		SpriteRenderer.sprite = FrameSprites[frameIndex];
+	}
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+
+		if (timer >= FrameTime)
+		{
+			frameIndex++;
+			if (frameIndex >= FrameSprites.Count)
+			{
+				frameIndex = 0;
+			}
+			timer = 0;
+			SpriteRenderer.sprite = FrameSprites[frameIndex];
+		}
+	}
+}
