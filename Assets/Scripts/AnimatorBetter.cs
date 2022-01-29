00a0589ad45d530dc5f8b7f086cd2c183aef122f
Szymon Piórkowski <szymon.piorkowski@vilemonarch.com>
2022-01-29 19:57:14 +0100

    Added animator and animation to żarówka

---
+++ Assets/Scripts/AnimatorBetter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorBetter : MonoBehaviour
+{
+
+	public List<Sprite> FrameSprites = new List<Sprite>();
+	public float FrameTime;
+
+	public SpriteRenderer renderer;
+
+	private int frameIndex;
+	private float timer;
+
+
+	private void Awake()
+	{
+		frameIndex = 0;
+		timer = 0;
+		renderer.sprite = FrameSprites[frameIndex];
+
+	}
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+
+		if (timer >= FrameTime)
+		{
+			frameIndex++;
+			if (frameIndex >= FrameSprites.Count)
+			{
+				frameIndex = 0;
+			}
+			timer = 0;
+			renderer.sprite = FrameSprites[frameIndex];
+		}
+	}
+}
