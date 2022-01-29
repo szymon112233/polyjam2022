888f46a0f4552cd6e4b00c9c74cf46c24c522a0e
Szymon Piórkowski <szymon.piorkowski@vilemonarch.com>
2022-01-29 13:51:33 +0100

    Basic node logic.

--- Assets/Scripts/Nodes/BasicNode.cs
+++ Assets/Scripts/Nodes/BasicNode.cs
@@ -1,6 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class BasicNode : MonoBehaviour
 {
-	public bool Activated = false;
+	public bool Activated
+	{
+		get
+		{
+			return _Activated;
+		}
+		set
+		{
+			_Activated = value;
+
+			if (value)
+			{
+				OnActivated.Invoke();
+			}
+			else
+			{
+				OnDeactivated.Invoke();
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Wave"))
+		{
+			Activated = true;
+		}
+	}
+	
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Wave"))
+		{
+			Activated = false;
+		}
+	}
+
+	private bool _Activated;
+
+	public UnityEvent OnActivated;
+	public UnityEvent OnDeactivated;
 }
