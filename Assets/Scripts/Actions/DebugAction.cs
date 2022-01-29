31523fabf7e8ff50475abee220bfca69dda96040
Sergey_Veram4uk <siarhei.veremchuk@vilemonarch.com>
2022-01-29 14:54:41 +0100

    Added Interactable prefab and DebugAction

---
+++ Assets/Scripts/Actions/DebugAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Actions
+{
+    [CreateAssetMenu(fileName = "DebugAction", menuName = "DebugAction", order = 0)]
+    public class DebugAction : BasicAction
+    {
+        public override void DoAction(BasicNode invokingNode, Vector3 direction)
+        {
+            Debug.Log($"{invokingNode.name} do action with direction {direction}");
+        }
+    }
+}
--- Assets/Scripts/Nodes/InteractiveNode.cs
+++ Assets/Scripts/Nodes/InteractiveNode.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 public class InteractiveNode : BasicNode
 {
 	public List<BasicAction> Actions;
-	
+
+	private void Awake()
+	{
+		OnActivated.AddListener(()=> Fire(transform.localRotation.eulerAngles)); 
+	}
+
 	public void Fire(Vector3 direction)
 	{
 		if (Actions.Count == 0)
